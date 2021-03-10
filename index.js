82ec78d89e67a61a226b60359c68e2eac861f396
mishuagopian <mishuagopian@gmail.com>
2021-03-09 21:30:26 -0300

    check for PORT env var on server setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const headers = [
   ">,MqBuN?$uS&+v:|t/2Yesgt4JJC,@",
